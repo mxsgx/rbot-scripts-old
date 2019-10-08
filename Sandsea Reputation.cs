955b5b35c3073b042be4e6ef8754161e55833503
Masga Satria Wirawan <45129872+mxsgx@users.noreply.github.com>
2019-10-08 22:24:29 +0700

    Rewrite Sandsea Reputation Bot
    Script written more efficiently.

---
+++ Sandsea Reputation.cs
@@ -0,0 +1,55 @@
+using RBot;
+
+public class Script {
+	public void CheckQuests( ScriptInterface bot ) {
+		for ( int i = 916; i < 926; i++ ) {
+			if ( ! bot.Quests.IsInProgress( i ) )
+				bot.Quests.EnsureAccept( i );
+
+			if ( bot.Quests.CanComplete( i ) ) {
+				bot.Quests.EnsureComplete( i );
+				bot.Quests.EnsureAccept( i );
+			}
+		}
+	}
+
+	public void GetMapItem( ScriptInterface bot ) {
+		bot.Map.GetMapItem( 247 );
+		bot.Map.GetMapItem( 248 );
+		bot.Map.GetMapItem( 249 );
+	}
+
+	public void ScriptMain( ScriptInterface bot ) {
+		bot.Options.SafeTimings = true;
+		bot.Options.RestPackets = true;
+		bot.Options.ExitCombatBeforeQuest = true;
+		bot.Options.HuntDelay = 500;
+		bot.Options.InfiniteRange = true;
+		
+		bot.Skills.Add( 1, 1 );
+		bot.Skills.Add( 2, 1 );
+		bot.Skills.Add( 3, 1 );
+		bot.Skills.Add( 4, 1 );
+		bot.Skills.StartTimer();
+		
+		while ( true ) {
+			if ( bot.Map.Name != "sandsea" )
+				bot.Player.Join( "sandsea" );
+			
+			CheckQuests( bot );
+			
+			string[] monsters = {
+				"Desert Vase",
+				"Bupers Camel",
+				"Sand Monkey",
+				"Cactus Creeper"
+			};
+
+			foreach ( string monster in monsters ) {
+				bot.Player.Hunt( monster );
+				GetMapItem( bot );
+				CheckQuests( bot );
+			}
+		}
+	}
+}
