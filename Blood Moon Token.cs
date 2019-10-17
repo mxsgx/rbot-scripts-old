09f778c0f9f84e4670a02163b6793e74dee1b473
Masga Satria Wirawan <45129872+mxsgx@users.noreply.github.com>
2019-10-17 09:25:42 +0700

    Blood Moon Token Farm Bot

---
+++ Blood Moon Token.cs
@@ -0,0 +1,56 @@
+using RBot;
+
+public class Script {
+	public ScriptInterface Bot;
+	public string[] Items;
+	
+	public void CheckItemsRequired() {
+		foreach ( string item in Items ) {
+			if ( Bot.Bank.Contains( item ) )
+				Bot.Bank.ToInventory( item );
+		}
+	}
+	public void CheckQuests() {
+		if ( Bot.Quests.CanComplete( 6059 ) )
+			Bot.Quests.EnsureComplete( 6059 );
+		
+		Bot.Quests.EnsureAccept( 6059 );
+	}
+	public void ScriptMain( ScriptInterface bot ){
+		Bot = bot;
+		Items = new string[] {
+			"Black Blood Vial",
+			"Moon Stone",
+			"Blood Moon Token"
+		};
+		
+		bot.Options.SafeTimings = true;
+		bot.Options.RestPackets = true;
+		bot.Options.ExitCombatBeforeQuest = true;
+		bot.Options.InfiniteRange = true;
+		bot.Drops.RejectElse = true;
+		
+		bot.Skills.Add( 1, 1 );
+		bot.Skills.Add( 2, 1 );
+		bot.Skills.Add( 3, 1 );
+		bot.Skills.Add( 4, 1 );
+		bot.Skills.StartTimer();
+		
+		foreach ( string item in Items )
+			bot.Drops.Add( item );
+		
+		bot.Drops.Start();
+		
+		CheckItemsRequired();
+		CheckQuests();
+		
+		while ( bot.Inventory.GetQuantity( "Blood Moon Token" ) < 300 ) {
+			if ( bot.Map.Name != "bloodmoon" )
+				bot.Player.Join( "bloodmoon", "r4a", "Left" );
+				
+			bot.Player.HuntForItem( "Black Unicorn", "Black Blood Vial", 1 );
+			bot.Player.HuntForItem( "Lycan Guard", "Moon Stone", 1 );
+			CheckQuests();
+		}
+	}
+}
