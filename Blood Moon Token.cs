b02d24f723b593e8f3a745b88c58e8e85f633413
Masga Satria Wirawan <45129872+mxsgx@users.noreply.github.com>
2019-10-17 10:17:10 +0700

    Fix disconnected issue after killing Lycan Guard

--- Blood Moon Token.cs
+++ Blood Moon Token.cs
@@ -10,12 +10,14 @@ public class Script {
 				Bot.Bank.ToInventory( item );
 		}
 	}
+	
 	public void CheckQuests() {
 		if ( Bot.Quests.CanComplete( 6059 ) )
 			Bot.Quests.EnsureComplete( 6059 );
 		
 		Bot.Quests.EnsureAccept( 6059 );
 	}
+	
 	public void ScriptMain( ScriptInterface bot ){
 		Bot = bot;
 		Items = new string[] {
@@ -23,30 +25,27 @@ public class Script {
 			"Moon Stone",
 			"Blood Moon Token"
 		};
-		
 		bot.Options.SafeTimings = true;
 		bot.Options.RestPackets = true;
 		bot.Options.ExitCombatBeforeQuest = true;
 		bot.Options.InfiniteRange = true;
-		bot.Drops.RejectElse = true;
-		
+		bot.Drops.RejectElse = false;
+		bot.Drops.Interval = 1500;
+
 		bot.Skills.Add( 1, 1 );
 		bot.Skills.Add( 2, 1 );
 		bot.Skills.Add( 3, 1 );
 		bot.Skills.Add( 4, 1 );
 		bot.Skills.StartTimer();
-		
-		foreach ( string item in Items )
-			bot.Drops.Add( item );
-		
+		bot.Drops.Add( "Blood Moon Token" );
 		bot.Drops.Start();
-		
+		bot.Player.LoadBank();
 		CheckItemsRequired();
 		CheckQuests();
 		
 		while ( bot.Inventory.GetQuantity( "Blood Moon Token" ) < 300 ) {
 			if ( bot.Map.Name != "bloodmoon" )
-				bot.Player.Join( "bloodmoon", "r4a", "Left" );
+				bot.Player.Join( "bloodmoon", "r17", "Bottom" );
 				
 			bot.Player.HuntForItem( "Black Unicorn", "Black Blood Vial", 1 );
 			bot.Player.HuntForItem( "Lycan Guard", "Moon Stone", 1 );
